798a8490562434f9c5b5036f1e0296ef1c88d1e7
Welberth Azevedo Barbosa <welberthab@gmail.com>
2023-10-24 16:08:01 -0300

    commit inicial

---
+++ Solution.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        result = 0
+        for counter,value in enumerate(nums):            
+            result ^= counter+1
+            result ^= value          
+        return result
