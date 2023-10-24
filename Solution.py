c5f33839afbc646b2ba85ded1f90d06f1629da27
Welberth Azevedo Barbosa <welberthab@gmail.com>
2023-10-24 16:10:39 -0300

    Solution

--- Solution.py
+++ Solution.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def missingNumber(self, nums):
         result = 0
-        for counter,value in enumerate(nums):            
+        for counter, value in enumerate(nums):
             result ^= counter+1
-            result ^= value          
+            result ^= value
         return result
